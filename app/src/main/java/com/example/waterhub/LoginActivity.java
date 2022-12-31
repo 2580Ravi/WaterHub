900b201addcd6ba26de71ce07ca752b399680de9
2580Ravi <koteravi.2580@gmail.com>
2022-12-31 19:38:38 +0530

    UI Activity Completed

---
+++ app/src/main/java/com/example/waterhub/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.waterhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    TextView register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        register = (TextView) findViewById(R.id.textView3);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/waterhub/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.waterhub;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.waterhub.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.idHome :
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.idAccount:
+                    replaceFragment(new AccountFragment());
+                    break;
+                case R.id.idCart:
+                    replaceFragment(new CartFragment());
+                    break;
+                case R.id.idFavourites:
+                    replaceFragment(new WishlistFragment());
+                    break;
+            }
+
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction  fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
